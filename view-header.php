0ef811616629d1620b4bad93ad8cea28503fe2a8
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-10-25 16:04:08 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -26,21 +26,17 @@
               </li>
               
               <li class="nav-item">
-                <a class="nav-link" href="post.php">Post</a>
+                <a class="nav-link" href="instructors.php">Instructors</a>
               </li>
               
               <li class="nav-item">
-                <a class="nav-link" href="post-result.php">Post Result</a>
+                <a class="nav-link" href="courses.php">Courses</a>
               </li>
               
               <li class="nav-item">
-                <a class="nav-link" href="get.php">Get</a>
+                <a class="nav-link" href="instructors-with-courses.php">Instructors with courses</a>
               </li> 
-              
-              <li class="nav-item">
-                <a class="nav-link" href="get-result.php">Get Result</a>
-              </li>
-              
+
             </ul>
           </div>
         </div>
