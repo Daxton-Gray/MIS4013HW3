2ba6fd568fa1976f1c09878e86d2cc3125a44857
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-10-28 13:31:28 -0500

    Update DB_Connection.php

--- DB_Connection.php
+++ DB_Connection.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $conn = new mysqli('dgwebdesignserver.mysql.database.azure.com', 'DGray', 'Dynodax0520@', 'dgwebdesignserver');
     
     // Check connection
     if ($conn->connect_error) {
