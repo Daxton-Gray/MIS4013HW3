4c1ee0cecd1b1a52c2d38aef143c06492df4c35a
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-06 11:56:53 -0600

    Update and rename DB_Connection.php to util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('dgwebdesignserver.mysql.database.azure.com', 'DGray', 'Dynodax0520@', 'dgwebdesignserver');
+    $conn = new mysqli('dgwebdesignserver.mysql.database.azure.com', 'DGray', 'Dynodax0520@', 'hw3_schema');
     
     // Check connection
     if ($conn->connect_error) {
