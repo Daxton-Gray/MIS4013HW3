57bfbbaed90e33589739710849f143268b56bc20
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-06 22:42:59 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -4,14 +4,9 @@ function get_db_connection(){
     $conn = new mysqli('dgwebdesignserver.mysql.database.azure.com', 'DGray@dgwebdesignserver', 'Connect@1234', 'hw3_schema');
     
     // Check connection
-    //if ($conn->connect_error) {
-      //return false;
-    //}
-    //return $conn;
-
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+      return false;
     }
-echo "Connected successfully";
+    return $conn;
 }
 ?>
