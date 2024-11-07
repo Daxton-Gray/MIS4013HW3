ecfb0ee7c54ec1445ac49db372032e783972f54e
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-06 22:51:41 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -1,13 +1,13 @@
 <?php
 
-//require_once("util-db.php");
-//require_once("model-teams.php");
+require_once("util-db.php");
+require_once("model-teams.php");
 
 $pageTitle = "Teams";
 include "view-header.php";
 
-//$teams = selectTeams();
-//include "view-teams.php";
+$teams = selectTeams();
+include "view-teams.php";
 
 include "view-footer.php";
 ?>
