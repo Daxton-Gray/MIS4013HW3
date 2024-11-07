1b75663f897a35e1174f1b9e27315f08e7ba5cc7
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-06 23:12:47 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -1,36 +1,13 @@
 <?php
 
-//require_once("util-db.php");
-//require_once("model-teams.php");
-
-//$pageTitle = "Teams";
-//include "view-header.php";
-
-//$teams = selectTeams();
-//include "view-teams.php";
-
-//include "view-footer.php";
-?>
-
-
-<?php
-
 require_once("util-db.php");
 require_once("model-teams.php");
 
 $pageTitle = "Teams";
 include "view-header.php";
 
-try {
-    // Attempt to fetch teams from the database
-    $teams = selectTeams();
-} catch (Exception $e) {
-    // If an error occurs, set teams to null and prepare an error message
-    $teams = null;
-    $errorMessage = "Error loading teams: " . htmlspecialchars($e->getMessage());
-}
-
+$teams = selectTeams();
 include "view-teams.php";
 
 include "view-footer.php";
-
+?>
