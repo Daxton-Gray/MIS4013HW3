576236b68e728009a46b6e7f4120148cc458723d
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-08 14:39:04 -0600

    Create view-teams.php

---
+++ view-teams.php
@@ -0,0 +1,27 @@
+<h1>Teams</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>City</th>
+        <th>League</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($team = $teams->fetch_assoc() {
+?>
+       <tr>
+         <td><?php echo $team['team_id']; ?></td>
+         <td><?php echo $team['team_name']; ?></td>
+         <td><?php echo $team['city']; ?></td>
+         <td><?php echo $team['league_abbreviation']; ?></td>
+       </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
