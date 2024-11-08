d8e4d52ff77d8609d5811e82e6ed3592c09296d8
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-08 13:08:10 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -1,1 +1,19 @@
-
+<?php
+  function selectTeams() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name, city, league_abbreviation FROM league LG JOIN team T ON LG.league_id=T.league_id JOIN location LC ON LC.location_id=T.location_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
