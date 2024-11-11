04907aa119dc86fd55c829332612f3a4aa3e11aa
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:18:49 -0600

    Update view-stars.php

--- view-stars.php
+++ view-stars.php
@@ -1,1 +1,27 @@
-
+<h1>Stars</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>League Name</th>
+        <th>Abbreviation</th>
+        <th>Sport</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($star = $stars->fetch_assoc()) {
+?>
+       <tr>
+         <td><?php echo $star['league_id']; ?></td>
+         <td><?php echo $star['league_name']; ?></td>
+         <td><?php echo $star['league_abbreviation']; ?></td>
+         <td><?php echo $star['sport']; ?></td>
+       </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
