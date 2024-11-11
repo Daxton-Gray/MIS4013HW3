048e13ccf96139607209fb1e4ce1ef2703a5e0ae
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:17:46 -0600

    Update model-stars.php

--- model-stars.php
+++ model-stars.php
@@ -1,1 +1,19 @@
-
+<?php
+  function selectStars() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM allstar");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
