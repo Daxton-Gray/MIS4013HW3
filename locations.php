9d83d1413e1cbbca3022bdc6138dbc8ff63fa041
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:00:31 -0600

    Update locations.php

--- locations.php
+++ locations.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-locations.php");
 
+$pageTitle = "Locations";
+include "view-header.php";
+$teams = selectLocations();
+include "view-locations.php";
+include "view-footer.php";
+?>
