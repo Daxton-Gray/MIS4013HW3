53e1a39aedc091f2134fbfe0f7a715608830c691
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:24:17 -0600

    Update model-stars.php

--- model-stars.php
+++ model-stars.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM allstar");
+        $stmt = $conn->prepare("select allstar_id, team_name, f_name, l_name, position, season_with_team, allstar_seasons from allstar A JOIN team T ON A.team_id=T.team_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
