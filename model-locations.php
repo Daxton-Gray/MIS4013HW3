46d370da549a2bd48a06346b8b2222b3f627e991
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:02:31 -0600

    Update model-locations.php

--- model-locations.php
+++ model-locations.php
@@ -1,1 +1,19 @@
-
+<?php
+  function selectLocations() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM location");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
