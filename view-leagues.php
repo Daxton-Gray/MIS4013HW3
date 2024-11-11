3c6acdf3c198a7ebee75a4e3aa58ed5afed7a5c7
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:13:41 -0600

    Update view-leagues.php

--- view-leagues.php
+++ view-leagues.php
@@ -1,1 +1,27 @@
-
+<h1>Leagues</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>League Name</th>
+        <th>Abbreviation</th>
+        <th>Sport</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($league = $leagues->fetch_assoc()) {
+?>
+       <tr>
+         <td><?php echo $league['league_id']; ?></td>
+         <td><?php echo $league['league_name']; ?></td>
+         <td><?php echo $league['league_abbreviation']; ?></td>
+         <td><?php echo $league['sport']; ?></td>
+       </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
