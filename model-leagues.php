c25c2a0bd4fe919aed69826da0a5161481d993b1
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:11:37 -0600

    Update model-leagues.php

--- model-leagues.php
+++ model-leagues.php
@@ -1,1 +1,19 @@
-
+<?php
+  function selectLeagues() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM league");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
