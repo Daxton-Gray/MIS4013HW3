352021538c592650e673a13efa853ec189eb7ad9
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-11 14:04:38 -0600

    Update view-locations.php

--- view-locations.php
+++ view-locations.php
@@ -1,1 +1,27 @@
-
+<h1>Locations</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>City</th>
+        <th>State</th>
+        <th>Country</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($location = $locations->fetch_assoc()) {
+?>
+       <tr>
+         <td><?php echo $location['location_id']; ?></td>
+         <td><?php echo $location['city']; ?></td>
+         <td><?php echo $location['state']; ?></td>
+         <td><?php echo $location['country']; ?></td>
+       </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
