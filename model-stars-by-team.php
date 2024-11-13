694a4429479251d654f6cf9a9d34644488a6d53f
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-13 14:20:55 -0600

    Update model-stars-by-team.php

--- model-stars-by-team.php
+++ model-stars-by-team.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT allstar_id, f_name, l_name, league_abbreviation, team_name, position, season_with_team, allstar_seasons FROM allstar A JOIN team T ON A.team_id=T.team_id JOIN league LG ON LG.league_id=T.league_id JOIN location LC ON LC.location_id=T.location_id WHERE A.team_id=?");
+        $stmt = $conn->prepare("SELECT allstar_id, f_name, l_name, league_abbreviation, city, team_name, position, season_with_team, allstar_seasons FROM allstar A JOIN team T ON A.team_id=T.team_id JOIN league LG ON LG.league_id=T.league_id JOIN location LC ON LC.location_id=T.location_id WHERE A.team_id=?");
         $stmt->bind_param("i", $tid);
         $stmt->execute();
         $result = $stmt->get_result();
