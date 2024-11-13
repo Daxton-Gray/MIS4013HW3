d1c9dd47796d802ccebcb4f3a86032d0fe8783fc
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-13 14:47:52 -0600

    Create teams-by-state.php

---
+++ teams-by-state.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-teams-by-state.php");
+
+$pageTitle = "Teams by State";
+include "view-header.php";
+$teams = selectTeamsByState($_POST['state']);
+include "view-teams-by-state.php";
+include "view-footer.php";
+?>
