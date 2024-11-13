265aef29884c53462d42be2b17f2d3c79b92767b
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-13 11:19:21 -0600

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -4,9 +4,9 @@
     <thead>
       <tr>
         <th>Team ID</th>
-        <th>Name</th>
         <th>City</th>
-        <th>League</th>
+        <th>Name</th>
+        <th>Year Established</th>
         <th></th>
       </tr>
     </thead>
@@ -16,9 +16,9 @@ while ($team = $teams->fetch_assoc()) {
 ?>
        <tr>
          <td><?php echo $team['team_id']; ?></td>
-         <td><?php echo $team['team_name']; ?></td>
          <td><?php echo $team['city']; ?></td>
-         <td><?php echo $team['league_abbreviation']; ?></td>
+         <td><?php echo $team['team_name']; ?></td>
+         <td><?php echo $team['year_established']; ?></td>
          <td><a href="allstar-and-team-info.php?id=<?php echo $team['team_id']; ?>">All-Stars</a></td>
        </tr>
 <?php
