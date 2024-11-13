a836b627bc2279fb33db0e172ecfb686c9b85315
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-13 15:20:59 -0600

    Update model-teams-by-state.php

--- model-teams-by-state.php
+++ model-teams-by-state.php
@@ -1,11 +1,11 @@
 <?php
-  function selectTeamsByState($sid) 
+  function selectTeamsByState($id) 
   {
     try
       {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT team_id, team_name, league_abbreviation, city, state, country FROM team T JOIN league LG ON LG.league_id=T.league_id JOIN location LC ON LC.location_id=T.location_id WHERE LC.state=?");
-        $stmt->bind_param("s", $sid);
+        $stmt->bind_param("s", $id);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
