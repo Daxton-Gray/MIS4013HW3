fe524a629a668fca0bc7e2918d308884ee6f0df3
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-13 14:51:28 -0600

    Create model-teams-by-state.php

---
+++ model-teams-by-state.php
@@ -0,0 +1,20 @@
+<?php
+  function selectTeamsByState($state) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name, league_abbreviation, city, state, country FROM team T JOIN league LG ON LG.league_id=T.league_id JOIN location LC ON LC.location_id=T.location_id WHERE LG.state=?");
+        $stmt->bind_param("s", $state);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
