d6b297c273611c90e34b60993c8ae411ff84462a
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-13 15:10:43 -0600

    Update teams-by-state.php

--- teams-by-state.php
+++ teams-by-state.php
@@ -4,7 +4,7 @@ require_once("model-teams-by-state.php");
 
 $pageTitle = "Teams by State";
 include "view-header.php";
-$teams = selectTeamsByState($_POST['state']);
+$teams = selectTeamsByState($_POST['sid']);
 include "view-teams-by-state.php";
 include "view-footer.php";
 ?>
