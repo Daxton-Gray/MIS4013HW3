ab944c1aa5d6e2aae91c99611ee7ffc0df80b2ae
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-14 12:39:51 -0600

    Create model-teams-by-league.php

---
+++ model-teams-by-league.php
@@ -0,0 +1,20 @@
+<?php
+  function selectTeamsByLeague($lid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, league_abbreviation city, team_name, year_established FROM team T JOIN location LC ON T.location_id=LC.location_id JOIN league LG ON T.league_id=LG.league_id WHERE T.league_id=?");
+        $stmt->bind_param("i", $lid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
