9dc04e157309ff92fc0cc5dadc24d98b7a6e8987
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-14 13:59:11 -0600

    Update view-stars-by-team.php

--- view-stars-by-team.php
+++ view-stars-by-team.php
@@ -1,9 +1,9 @@
-<h1>Stars By Team</h1>
+<h1>MVPs By Team</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-        <th>All-Star ID</th>
+        <th>MVP ID</th>
         <th>First Name</th>
         <th>Last Name</th>
         <th>League</th>
@@ -11,23 +11,23 @@
         <th>Team Name</th>
         <th>Position</th>
         <th>Seasons on Team</th>
-        <th>All-Star Seasons</th>
+        <th>MVP Seasons</th>
       </tr>
     </thead>
     <tbody>
 <?php
-while ($star = $stars->fetch_assoc()) {
+while ($mvp = $mvps->fetch_assoc()) {
 ?>
        <tr>
-         <td><?php echo $star['allstar_id']; ?></td>
-         <td><?php echo $star['f_name']; ?></td>
-         <td><?php echo $star['l_name']; ?></td>
-         <td><?php echo $star['league_abbreviation']; ?></td>
-         <td><?php echo $star['city']; ?></td>
-         <td><?php echo $star['team_name']; ?></td>
-         <td><?php echo $star['position']; ?></td>
-         <td><?php echo $star['season_with_team']; ?></td>
-         <td><?php echo $star['allstar_seasons']; ?></td>
+         <td><?php echo $mvp['mvp_id']; ?></td>
+         <td><?php echo $mvp['f_name']; ?></td>
+         <td><?php echo $mvp['l_name']; ?></td>
+         <td><?php echo $mvp['league_abbreviation']; ?></td>
+         <td><?php echo $mvp['city']; ?></td>
+         <td><?php echo $mvp['team_name']; ?></td>
+         <td><?php echo $mvp['position']; ?></td>
+         <td><?php echo $mvp['season_with_team']; ?></td>
+         <td><?php echo $mvp['mvp_seasons']; ?></td>
        </tr>
 <?php
 }
