e9a1805fd287b0aeab19d4e5ca06939b8a234abf
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-14 14:07:26 -0600

    Update model-mvps.php

--- model-mvps.php
+++ model-mvps.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select mvp_id, team_name, f_name, l_name, position, season_with_team, mvp_seasons from mvp M JOIN team T ON M.team_id=T.team_id");
+        $stmt = $conn->prepare("select mvp_id, team_name, f_name, l_name, position, seasons_with_team, mvp_seasons from mvp M JOIN team T ON M.team_id=T.team_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
