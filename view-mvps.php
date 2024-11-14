acd70fd9977a3d8501a6731e5b6c5d5f67cf5ff9
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-14 13:54:30 -0600

    Update and rename view-stars.php to view-mvps.php

---
+++ view-mvps.php
@@ -0,0 +1,33 @@
+<h1>MVPs</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>MVP ID</th>
+        <th>Team Name</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+        <th>Seasons on Team</th>
+        <th>MVP Seasons</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($mvp = $mvps->fetch_assoc()) {
+?>
+       <tr>
+         <td><?php echo $mvp['mvp_id']; ?></td>
+         <td><?php echo $mvp['team_name']; ?></td>
+         <td><?php echo $mvp['f_name']; ?></td>
+         <td><?php echo $mvp['l_name']; ?></td>
+         <td><?php echo $mvp['position']; ?></td>
+         <td><?php echo $mvp['season_with_team']; ?></td>
+         <td><?php echo $mvp['mvp_seasons']; ?></td>
+       </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
