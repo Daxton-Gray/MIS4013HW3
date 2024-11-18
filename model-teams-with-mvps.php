a3b222c5cd1b94186a79ca986bebd1392ff5fe82
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-18 13:30:14 -0600

    Update model-teams-with-mvps.php

--- model-teams-with-mvps.php
+++ model-teams-with-mvps.php
@@ -16,4 +16,23 @@
         throw $e;
       }
   }
+
+function selectMVPsByTeam($tid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT mvp_id, f_name, l_name, league_abbreviation, city, team_name, position, seasons_with_team, mvp_seasons FROM mvp M JOIN team T ON M.team_id=T.team_id JOIN league LG ON LG.league_id=T.league_id JOIN location LC ON LC.location_id=T.location_id WHERE M.team_id=?");
+        $stmt->bind_param("i", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
 ?>
