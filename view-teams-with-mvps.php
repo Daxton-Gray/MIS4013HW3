9ccb9c9e9e87c4c29f73e5267e3f64bcd1a1fdf3
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-18 15:30:20 -0600

    Update view-teams-with-mvps.php

--- view-teams-with-mvps.php
+++ view-teams-with-mvps.php
@@ -7,15 +7,18 @@ while ($team = $teams->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $team['team_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $mvps = selectMVPsByTeam($team['team_id']);
   while ($mvp = $mvps->fetch_assoc()) {
 ?>
+    <li class="list-group-item"><?php echo $mvp['f_name']; ?> <?php echo $mvp['l_name']; ?> - <?php echo $mvp['position']; ?> - <?php echo $mvp['season_with_team']; ?> Seasons - <?php echo $mvp['mvp_seasons']; ?> MVPs</li>
 <?php
   }
 ?>
+      </ul>
       </p>
-      <p class="card-text"><small class="text-body-secondary">Est. <?php echo $team['year_established']; ?></small></p>
+      <p class="card-text"><small class="text-body-secondary">Joined <?php echo $mvp['league_abbreviation']; ?> <?php echo $team['year_established']; ?></small></p>
     </div>
   </div>
 
