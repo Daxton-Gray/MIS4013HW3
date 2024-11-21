bd2533d092e275ff534e93153c1c71af33c43dd7
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-21 12:17:12 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
+    case "Delete":
+      if (deleteTeam($_POST['team_id'])) {
+        echo '<div class="alert alert-success" role="alert">Team Deleted</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
   }
 }
 
