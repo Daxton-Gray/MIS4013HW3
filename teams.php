bac9ba81dab42307ac795b9995f08b9e36b1f851
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-21 14:33:41 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertTeam($_POST['location_id'], $_POST['league_id'], $_POST['tName'], $_POST['yearEst'])) {
+      if (insertTeam($_POST['locid'], $_POST['lgid'], $_POST['tName'], $_POST['yearEst'])) {
         echo '<div class="alert alert-success" role="alert">Team Added</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Delete":
-      if (deleteTeam($_POST['team_id'])) {
+      if (deleteTeam($_POST['tid'])) {
         echo '<div class="alert alert-success" role="alert">Team Deleted</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
