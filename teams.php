d6e5630e859d6ef7e8f617ed5161869e166dd165
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-20 20:20:59 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -4,6 +4,15 @@ require_once("model-teams.php");
 
 $pageTitle = "Teams";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertTeam($_POST['location_id'], $_POST['league_id'], $_POST['tName'], $_POST['yearEst']);
+      break;
+  }
+}
+
 $teams = selectTeams();
 include "view-teams.php";
 include "view-footer.php";
