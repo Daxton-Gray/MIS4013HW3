1a136dea51315398d794c24fa871a2667f0e5d64
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-22 12:07:10 -0600

    Update model-locations.php

--- model-locations.php
+++ model-locations.php
@@ -16,4 +16,59 @@
         throw $e;
       }
   }
+
+function insertLocation($city, $state, $country) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hw3_schema`.`location` (`location_id`, `city`, `state`, `country`) VALUES (NULL, ?, ?, ?)");
+        $stmt->bind_param("sss", $city, $state, $country);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function updateLocation($city, $state, $country, $locid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `hw3_schema`.`location` SET city = ?, state = ?, country = ? WHERE location_id = ?");
+        $stmt->bind_param("sssi", $city, $state, $country, $locid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function deleteLocation($locid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `hw3_schema`.`location` WHERE location_id = ?");
+        $stmt->bind_param("i", $locid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
 ?>
