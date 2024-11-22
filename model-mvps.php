4f7695d3a073e90b0337338c201c567b38eb9af7
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-22 15:08:07 -0600

    Update model-mvps.php

--- model-mvps.php
+++ model-mvps.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select mvp_id, team_name, f_name, l_name, position, seasons_with_team, mvp_seasons from mvp M JOIN team T ON M.team_id=T.team_id");
+        $stmt = $conn->prepare("select mvp_id, team_name, f_name, l_name, position, seasons_with_team, mvp_seasons, M.team_id from mvp M JOIN team T ON M.team_id=T.team_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -16,4 +16,59 @@
         throw $e;
       }
   }
+
+function insertMVP($team_id, $f_name, $l_name, $position, $seasons_with_team, $mvp_seasons) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hw3_schema`.`mvp` (`mvp_id`, `team_id`, `f_name`, `l_name`, `position`, `seasons_with_team`, `mvp_seasons`) VALUES (NULL, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("isssii", $team_id, $f_name, $l_name, $position, $seasons_with_team, $mvp_seasons);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function updateMVP($team_id, $f_name, $l_name, $position, $seasons_with_team, $mvp_seasons, $mvpid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `hw3_schema`.`mvp` SET `team_id` = ?, `f_name` = ?, `l_name` = ?, `position` = ?, `seasons_with_team` = ?, `mvp_seasons` = ? WHERE `mvp_id` = ?");
+        $stmt->bind_param("sssi", $league_name, $league_abbreviation, $sport, $lgid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function deleteMVP($mvpid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `hw3_schema`.`mvp` WHERE mvp_id = ?");
+        $stmt->bind_param("i", $mvpid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
 ?>
