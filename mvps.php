02c78fd63947dc14b692a4a36dfcdc8a6565f329
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-22 15:38:07 -0600

    Update mvps.php

--- mvps.php
+++ mvps.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertMVP($_POST['tid'], $_POST['f_name'], $_POST['l_name'], $_POST['position'], $_POST['seasons_with_team'], $_POST['mvp_seasons'])) {
+      if (insertMVP($_POST['team_id'], $_POST['f_name'], $_POST['l_name'], $_POST['position'], $_POST['seasons_with_team'], $_POST['mvp_seasons'])) {
         echo '<div class="alert alert-success" role="alert">MVP Added</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Edit":
-      if (updateMVP($_POST['tid'], $_POST['f_name'], $_POST['l_name'], $_POST['position'], $_POST['seasons_with_team'], $_POST['mvp_seasons'], $_POST['mvpid'])) {
+      if (updateMVP($_POST['team_id'], $_POST['f_name'], $_POST['l_name'], $_POST['position'], $_POST['seasons_with_team'], $_POST['mvp_seasons'], $_POST['mvpid'])) {
         echo '<div class="alert alert-success" role="alert">MVP Edited</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
