c0815f2e2201ad0cf6f60059a3aac101325b0d33
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-24 22:55:07 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -17,6 +17,24 @@
       }
   }
 
+  function selectLocationsForInput() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM location ORDER BY state");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
   function insertTeam($locid, $lgid, $tName, $yearEst) 
   {
     try
