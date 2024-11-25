8511264dad8b14516d3c0ff04af438c492cd18b8
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-24 23:04:07 -0600

    Update view-location-input-list.php

--- view-location-input-list.php
+++ view-location-input-list.php
@@ -1,1 +1,13 @@
-
+<select class="form-select" id="locid" name="locid">
+<?php
+while ($locationItem = $locationList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedLocation == $locationItem['location_id']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $locationItem['location_id']; ?>"<?=$selText?>><?php echo $locationItem['city']; ?> <?php echo $locationItem['state']; ?></option>
+<?php
+}
+?>
+</select>
