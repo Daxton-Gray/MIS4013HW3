5c09e5e796bbdfb1d7c545f087e8877e5196ad5f
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-25 17:19:15 -0600

    Update model-teams-by-league.php

--- model-teams-by-league.php
+++ model-teams-by-league.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT team_id, league_abbreviation, city, team_name, year_established FROM team T JOIN location LC ON T.location_id=LC.location_id JOIN league LG ON T.league_id=LG.league_id WHERE T.league_id=?");
+        $stmt = $conn->prepare("SELECT team_id, league_abbreviation, city, team_name, year_established, T.location_id, T.league_id FROM team T JOIN location LC ON T.location_id=LC.location_id JOIN league LG ON T.league_id=LG.league_id WHERE T.league_id=?");
         $stmt->bind_param("i", $lid);
         $stmt->execute();
         $result = $stmt->get_result();
