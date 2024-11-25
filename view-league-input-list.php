b02c00b932137ee20cd6ecea418bdb1d23a2b837
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-24 23:02:33 -0600

    Update view-league-input-list.php

--- view-league-input-list.php
+++ view-league-input-list.php
@@ -1,1 +1,13 @@
-
+<select class="form-select" id="lgid" name="lgid">
+<?php
+while ($leagueItem = $leagueList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedLeague == $leagueItem['league_id']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $leagueItem['league_id']; ?>"<?=$selText?>><?php echo $leagueItem['league_abbreviation']; ?></option>
+<?php
+}
+?>
+</select>
