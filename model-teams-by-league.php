b45f2b6dcda16f360bcee95f3a9aff94a22b8b02
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-25 17:55:55 -0600

    Update model-teams-by-league.php

--- model-teams-by-league.php
+++ model-teams-by-league.php
@@ -1,11 +1,11 @@
 <?php
-  function selectTeamsByLeague($lid) 
+  function selectTeamsByLeague($lgid) 
   {
     try
       {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT team_id, league_abbreviation, city, team_name, year_established, T.location_id, T.league_id FROM team T JOIN location LC ON T.location_id=LC.location_id JOIN league LG ON T.league_id=LG.league_id WHERE T.league_id=?");
-        $stmt->bind_param("i", $lid);
+        $stmt->bind_param("i", $lgid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
