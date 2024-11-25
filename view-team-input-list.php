a855234be77bdae9a0fdfc14e1526f1966dd43cf
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-24 22:29:30 -0600

    Create view-team-input-list.php

---
+++ view-team-input-list.php
@@ -0,0 +1,9 @@
+<select class="form-select" id="team_id" name="team_id">
+<?php
+while ($teamItem = $teamList->fetch_assoc()) {
+?>
+  <option value="<?php echo $teamItem['team_id']; ?>">"<?php echo $teamItem['team_name']; ?></option>
+<?php
+}
+?>
+</select>
