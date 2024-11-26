c7ab429cff8772cc39b5cb8bd1c9f8319ae74808
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-11-25 18:17:47 -0600

    Update teams-with-mvps.php

--- teams-with-mvps.php
+++ teams-with-mvps.php
@@ -4,6 +4,33 @@ require_once("model-teams-with-mvps.php");
 
 $pageTitle = "Teams with MVPs";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertMVP($_POST['team_id'], $_POST['f_name'], $_POST['l_name'], $_POST['position'], $_POST['seasons_with_team'], $_POST['mvp_seasons'])) {
+        echo '<div class="alert alert-success" role="alert">MVP Added</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateMVP($_POST['team_id'], $_POST['f_name'], $_POST['l_name'], $_POST['position'], $_POST['seasons_with_team'], $_POST['mvp_seasons'], $_POST['mvpid'])) {
+        echo '<div class="alert alert-success" role="alert">MVP Edited</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteMVP($_POST['mvpid'])) {
+        echo '<div class="alert alert-success" role="alert">MVP Deleted</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
 $teams = selectTeams();
 include "view-teams-with-mvps.php";
 include "view-footer.php";
